1546822b89772a0798b83e25f3be12f35f66f834
EDUARDO Jorge Andrade Mourao Oliveira B901599 <B901599@bnb.gov.br>
2025-05-16 16:29:59 -0300

    adding constructor

--- Persistence/Context/AppDbContext.cs
+++ Persistence/Context/AppDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchTemplate.Persistence.Context
 {
     public class AppDbContext: DbContext
     {
-        public AppDbContext() {  }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {  }
     }
 }
