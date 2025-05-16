5ff478efdac1e63a17d3fc93f5097b9f864d013f
EDUARDO Jorge Andrade Mourao Oliveira B901599 <B901599@bnb.gov.br>
2025-05-16 16:17:18 -0300

    Initial commit

---
+++ CleanArchTemplate.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchTemplate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchTemplate.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T: BaseEntity
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> Get(Guid id, CancellationToken cancellationToken);
+        Task<List<T>> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ CleanArchTemplate.Domain/Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchTemplate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchTemplate.Domain.Interfaces
+{
+    public interface IUserRepository: IBaseRepository<User>
+    {
+        Task<User> GetByEmail(string email, CancellationToken cancellationToken);
+    }
+}
