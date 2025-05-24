e30878950fb9b3329ab1c3c50238984d68347614
Eduardo Andrade <pewpola>
2025-05-24 19:30:41 -0300

    Creating ServiceExtensions

---
+++ CleanArchTemplate.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchTemplate.Application.Services
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
