e50480a869eda6ea9a6f1f01f8f21f361fe105be
Eduardo Andrade <pewpola>
2025-05-24 18:35:40 -0300

    Handler 70% and Validator

---
+++ CleanArchTemplate.Application/UseCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace CleanArchTemplate.Application.UseCases.CreateUser
+{
+    public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>
+    {
+        public CreateUserValidator() 
+        {
+            RuleFor(request => request.Email).NotEmpty().MaximumLength(50).EmailAddress();
+            RuleFor(request => request.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+        }
+    }
+}
