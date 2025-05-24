df0ffc02f4d081234b33eb05a5ec0c5c12e4e566
Eduardo Andrade <pewpola>
2025-05-24 17:22:37 -0300

    Creating Mapper, Request and Responde for User

---
+++ CleanArchTemplate.Application/UseCases/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace CleanArchTemplate.Application.UseCases.CreateUser
+{
+    public sealed record CreateUserRequest(string Email, String Name) : IRequest<CreateUserResponse>;
+    {
+    }
+}
