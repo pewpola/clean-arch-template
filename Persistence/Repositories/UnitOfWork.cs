988b0d7c9f80128709a1211ab3e20954eb279266
Eduardo Andrade <pewpola>
2025-05-24 15:05:13 -0300

    creating UnitOfWork and ServicesExtensions

---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchTemplate.Persistence.Repositories
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task Commit(CancellationToken cancellationToken)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Persistence/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchTemplate.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchTemplate.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
