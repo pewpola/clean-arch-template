b47b5504a82d440d163db2e026df4d75cb0ae3e3
EDUARDO Jorge Andrade Mourao Oliveira B901599 <B901599@bnb.gov.br>
2025-05-27 15:15:12 -0300

    starting delete use case

---
+++ CleanArchTemplate.Application/UseCases/DeleteUser/DeleteUserMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchTemplate.Application.UseCases.DeleteUser
+{
+    internal class DeleteUserMapper
+    {
+    }
+}
